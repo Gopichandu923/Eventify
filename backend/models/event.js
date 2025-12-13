5bcbdc9643fbe7dca18d953ec52b674a5e649f3d
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-13 23:15:02 +0530

    Created event model for storing events data.

---
+++ backend/models/event.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true },
+    venue: { type: String, required: true },
+    ticketLimit: { type: Number, required: true, default: 0 },
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    approvalMethod: {
+      type: String,
+      enum: ["auto", "manual"],
+      default: "auto",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Event", eventSchema);
