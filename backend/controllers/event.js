1dcfc596d4851838dea7f52ca3a991484e2f7e17
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-14 16:32:01 +0530

    Updated event controler.

--- backend/controllers/event.js
+++ backend/controllers/event.js
@@ -1,4 +1,5 @@
 import EventModel from "../models/event.js";
+import RegistrationModel from "../models/registration.js";
 
 //POST create event
 export const createEvent = async (req, res) => {
@@ -29,6 +30,57 @@ export const createEvent = async (req, res) => {
     return res.status(201).json(newEvent);
   } catch (error) {
     console.log("Error during event creation : " + error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+//GET to get all events of an organizer
+export const getMyEvents = async (req, res) => {
+  try {
+    const events = await EventModel.find({ organizer: req.user.id });
+    return res.status(200).json(events);
+  } catch (error) {
+    console.log("Error retrieving all events of an organizer:" + error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+//GET get event details
+export const getEventDetails = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ message: "Please provide event id." });
+  }
+  try {
+    const event = await EventModel.findOne({ _id: id }).select("-organizer");
+    if (!event) {
+      return res.status(404).json({ message: `Event not found with id ${id}` });
+    }
+    return res.status(200).json(event);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+//GET get event registrations of an event
+export const getRegistrations = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ message: "Please provide event id." });
+  }
+  try {
+    const event = await EventModel.findOne({ _id: id });
+
+    if (!event || event.organizer.toString() !== req.user.id) {
+      return res
+        .status(404)
+        .json({ message: "Event not found or unauthorized" });
+    }
+    const registrations = await RegistrationModel.find({ event: id });
+    return res.status(200).json({ event, registrations });
+  } catch (error) {
+    console.log(error.message);
     return res.status(500).json({ message: "Internal server error." });
   }
 };
