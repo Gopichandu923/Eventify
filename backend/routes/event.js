2e64d9d0a4aa6b6ea200e0c0e290611dfeafdfab
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-14 16:33:53 +0530

    Updated the event routes.

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createEvent } from "../controllers/event.js";
+import {
+  createEvent,
+  getEventDetails,
+  getMyEvents,
+  getRegistrations,
+} from "../controllers/event.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createEvent);
-
+router.get("/", protect, getMyEvents);
+router.get("/:id", getEventDetails);
+router.get("/:id/registrations", protect, getRegistrations);
 export default router;
