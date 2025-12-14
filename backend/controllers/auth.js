6710a2984d153e4ce7bb3808df5e18a073683285
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-14 11:02:46 +0530

    Created the signin controller.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,9 @@
 import UserModel from "../models/user.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const generateJwt = (id) => {
   const payload = { user: { id: id } };
@@ -18,21 +21,53 @@ export const signupUser = async (req, res) => {
       .json({ message: "Please provide name , email and password." });
   }
   try {
-    const user = await UserModel.find({ email: email });
+    const user = await UserModel.findOne({ email: email });
     if (user) {
-      res.status(400).json({
+      return res.status(400).json({
         message: "User already exist with this email.Signup with new email.",
       });
     }
-    const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS);
-    const newUser = new UserModel({ name, email, hashedPassword });
+    const saltRounds = parseInt(process.env.SALT_ROUNDS);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = new UserModel({ name, email, password: hashedPassword });
     await newUser.save();
     const token = generateJwt(newUser._id);
-    res
-      .status(201)
-      .json({ message: "Successfully ", user: { name, email }, token: token });
+    return res.status(201).json({
+      user: { name, email },
+      token: token,
+    });
   } catch (error) {
     console.log("Error while creating new user :" + error);
-    res.status(500).json({ message: "Internal server error." });
+    return res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+//POST user signin
+export const signinUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(401)
+      .json({ message: "Please provide email and password." });
+  }
+  try {
+    const user = await UserModel.findOne(
+      { email: email },
+      { name: 1, password: 1 }
+    );
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+    const token = generateJwt(user._id);
+    return res
+      .status(200)
+      .json({ user: { name: user.name, email }, token: token });
+  } catch (error) {
+    console.log("Error while creating new user :" + error);
+    return res.status(500).json({ message: "Internal server error." });
   }
 };
