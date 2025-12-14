8895d6654892ebd41aee7fc7becc4bfccba43a81
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-14 11:03:26 +0530

    Created the login auth route.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
-import { signupUser } from "../controllers/auth.js";
+import { signupUser, signinUser } from "../controllers/auth.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/register", signupUser);
+router.post("/login", signinUser);
 
 export default router;
