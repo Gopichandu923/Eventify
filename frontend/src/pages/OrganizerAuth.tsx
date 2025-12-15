8dcead6d6b3457ad558ec1e4eeb2439c7c544eb0
Gopichandu923 <gopichandu6080@gmail.com>
2025-12-15 07:24:52 +0530

    Add new page components: CreateEvent, EventRegistrations, Home, OrganizerAuth, OrganizerDashboard, PublicEvent, and TicketPage

---
+++ frontend/src/pages/OrganizerAuth.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrganizerAuth = () => {
+  return <div>OrganizerAuth</div>;
+};
+
+export default OrganizerAuth;
---
+++ frontend/src/pages/PublicEvent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PublicEvent = () => {
+  return <div>PublicEvent</div>;
+};
+
+export default PublicEvent;
